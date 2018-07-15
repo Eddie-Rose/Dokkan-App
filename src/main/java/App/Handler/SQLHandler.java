b5f646027eee4790bf9dfbf0beb5bd2bb7a24345
Edwin Roesli <eddie.roesli@gmail.com>
2018-07-15 13:25:09 +1200

    sqlhandler

--- src/main/java/App/Handler/SQLHandler.java
+++ src/main/java/App/Handler/SQLHandler.java
@@ -1,6 +1,60 @@
 package App.Handler;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+
 public class SQLHandler {
+
+    private final String hostName = "eddiesserver.database.windows.net";
+    private final String dbName = "DokkanData";
+    private final String user = "dokkanreader";
+    private final String password = "BackingBakh9000";
+    private final String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+    Connection connection = null;
+
+    public static void main(String[] args) {
+
+        String hostName = "eddiesserver.database.windows.net";
+        String dbName = "DokkanData";
+        String user = "dokkanreader";
+        String password = "BackingBakh9000";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+
+            System.out.println("Query data example:");
+            System.out.println("=========================================");
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT * "
+                    + "FROM STATS AS pc ";
+
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+                // Print results from select statement
+                System.out.println("Stats:");
+                while (resultSet.next())
+                {
+                    System.out.println(resultSet.getString(1) + " "
+                            + resultSet.getString(2));
+                }
+                connection.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
